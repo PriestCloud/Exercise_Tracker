fe6d5633a6e67b05c0f11dd645757dbb8dec9fb3
Priest <desoyeolayinka@gmail.com>
2020-10-27 11:30:54 +0100

    update

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,27 @@
 const express = require('express');
-const port = 3000;
 const app = express();
 
-app.get('/user', function(req, res){
-    res.send("Working");
+const mongoose = require('mongoose');
+const url = 'mongodb://localhost/Exercise_Tracker_DB';
+mongoose.connect(url, {useNewUrlParser:true, useUnifiedTopology: true }, function(){
+    console.log("Call Me Back Baby")
+});
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("Part 2")
 })
+
+const cors = require('cors');
+require('dotenv').config();
+
+const port = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+// app.get('/user', function(req, res){
+//     res.send("Working");
+// })
 var server = app.listen(port, function(){
-    console.log("Priest First Backend Server!")
+    console.log("Priest Server!")
 }) 
