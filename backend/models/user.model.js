63d99342a6e3b4226b3d38d61842c9f4c3f50bf1
Priest <desoyeolayinka@gmail.com>
2020-10-28 21:50:32 +0100

    A very long Day manz.

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,15 +1,16 @@
 const mongoose = require('mongoose');
 
-const Schema = mongoose.Schema();
-const userSchema = new Schema({
+const userSchema = mongoose.Schema({
     username:{
-        type:string,
+        type:String,
         required:true,
         unique:true,
         trim:true,
-        minlength:5
+        minlength:5,
             }
         },
     {
         timestamps:true,
     });
+const User = mongoose.model('User', userSchema);
+module.exports = User;
--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,55 @@
 const express = require('express');
 const app = express();
 
+const port = 3000;
+
 const mongoose = require('mongoose');
-const url = 'mongodb://localhost/Exercise_Tracker_DB';
-mongoose.connect(url, {useNewUrlParser:true, useUnifiedTopology: true }, function(){
-    console.log("Call Me Back Baby");
-})
-const connection = mongoose.connection;
-connection.once('open', function () {
-    console.log("MongoDB database connection established successfully!");
-})
+
+const dbConfig = require('./config/db.config.js');
+const db = {};
+
+db.url = dbConfig.url;
+
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/mydb";
+
+mongoose.connect(db.url, function(err, db) {
+  if (err) throw err;
+  console.log("Database created!");
+  db.close();
+});
+
+
+//(C:\Users\The priest\node_modules\mongodb\lib\mongo_client.js:219:10)
+// mongoose.connect(db.url, {useNewUrlParser:true, useUnifiedTopology: true }, function(err, db) {
+//     console.log("Call Me Back Baby"); 
+//     if (err) throw err;
+//     console.log("Database created!");
+//     db.close();
+//     //console.log(mongoose.connection.readyState + "   11");
+// });
+
+
+// const connection = mongoose.connection;
+
+// connection.on('Connected', function () {
+//     console.log("MongoDB database connection established successfully!");
+//     console.log(mongoose.connection.readyState + "    12");
+// });
+
 
 const cors = require('cors');
-require('dotenv').config();
 
-const port = 3000;
+
 
 app.use(cors());
 app.use(express.json());
+
+const exerciseRouter = require('./routes/exercises');
+const userRouter = require('./routes/users');
+
+app.use('/exercises', exerciseRouter);
+app.use('/users', userRouter);
 
 // app.get('/user', function(req, res){
 //     res.send("Working");
