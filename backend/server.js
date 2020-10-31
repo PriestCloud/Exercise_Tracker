9aa20702f02b4b446e6003202ec2f0f911095e8d
Priest <desoyeolayinka@gmail.com>
2020-10-31 15:04:06 +0100

    update~!

--- backend/server.js
+++ backend/server.js
@@ -2,20 +2,17 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 const port = 5000;
-
+const cors = require('cors');
+app.use(cors());
+app.use(express.json());
 const dbConfig = require('./config/db.config.js');
 const db = {};
 db.url = dbConfig.url;
 
-const connection = new mongoose.connect(db.url, {useNewUrlParser:true, useUnifiedTopology: true }, function(err, db) {
+
+const mycon = new mongoose.connect(db.url, {useNewUrlParser:true, useUnifiedTopology: true }, function(err, db) {
     console.log(mongoose.connection.readyState + "   11");
 });
-
-
-const cors = require('cors');
-
-app.use(cors());
-app.use(express.json());
 
 const exerciseRouter = require('./routes/exercises');
 const userRouter = require('./routes/users');
@@ -25,6 +22,7 @@ app.use('/users', userRouter);
 app.get('/user', function(req, res){
     res.send("Working");
 })
+
 var server = app.listen(port, function(){
     console.log("Priest Server running !");
 });  
