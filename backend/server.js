fa90fb913b0a394029f246003c81cb9807a708c7
Priest <desoyeolayinka@gmail.com>
2020-10-31 14:22:46 +0100

    Testing

--- backend/server.js
+++ backend/server.js
@@ -10,32 +10,32 @@ const db = {};
 
 db.url = dbConfig.url;
 
-var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/mydb";
+// var MongoClient = require('mongodb').MongoClient;
+// var url = "mongodb://localhost:27017/mydb";
 
-mongoose.connect(db.url, function(err, db) {
-  if (err) throw err;
-  console.log("Database created!");
-  db.close();
+// mongoose.connect(db.url, function(err, db) {
+//   if (err) throw err;
+//   console.log("Database created!");
+//   db.close();
+// });
+
+
+// (C:\Users\The priest\node_modules\mongodb\lib\mongo_client.js:219:10)
+mongoose.connect(db.url, {useNewUrlParser:true, useUnifiedTopology: true }, function(err, db) {
+    console.log("Call Me Back Baby"); 
+    // if (err) throw err;
+    console.log("Database created!");
+    db.close();
+    //console.log(mongoose.connection.readyState + "   11");
 });
 
 
-//(C:\Users\The priest\node_modules\mongodb\lib\mongo_client.js:219:10)
-// mongoose.connect(db.url, {useNewUrlParser:true, useUnifiedTopology: true }, function(err, db) {
-//     console.log("Call Me Back Baby"); 
-//     if (err) throw err;
-//     console.log("Database created!");
-//     db.close();
-//     //console.log(mongoose.connection.readyState + "   11");
-// });
+const connection = mongoose.connection;
 
-
-// const connection = mongoose.connection;
-
-// connection.on('Connected', function () {
-//     console.log("MongoDB database connection established successfully!");
-//     console.log(mongoose.connection.readyState + "    12");
-// });
+connection.on('Connected', function () {
+    console.log("MongoDB database connection established successfully!");
+    console.log(mongoose.connection.readyState + "    12");
+});
 
 
 const cors = require('cors');
